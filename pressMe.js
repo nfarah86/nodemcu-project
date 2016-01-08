b2be364b8f4e19a157092a9125702b8a5fa7cb7f
Nadine <nfarah86@gmail.com>
2016-01-07 16:06:07 -0800

    edited: able to make a get request to the server created by nodemcu

--- pressMe.js
+++ pressMe.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
    $(".buttonClass").click(function(){
    		var buttonClicked = this.id;
    		alert(buttonClicked);
-   		$.get("http://192.168.4.1:80/", {piezoBuzzer:buttonClicked});
+   		$.get("http://192.168.1.122:80/", {piezoBuzzer:buttonClicked});
+   		//above is the ip address of the server I'm trying to hit
 	});
 });
